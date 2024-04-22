20b0a64ac480295c66e1958d59420bebaf2f5676
carlosjrs006 <carlosjrs06.cj@gmail.com>
2024-04-22 12:11:19 -0300

    ajuste no header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+
+  constructor(private router: Router, private elementRef: ElementRef) { }
+
+  collapseNavbar(): void {
+    const navbarToggler = this.elementRef.nativeElement.querySelector('.navbar-toggler');
+    const navbarCollapse = this.elementRef.nativeElement.querySelector('.navbar-collapse');
+
+    if (navbarToggler && navbarCollapse) {
+      const isCollapsed = navbarCollapse.classList.contains('show');
+      if (isCollapsed) {
+        navbarToggler.click(); // Fecha o menu
+      }
+    }
+  }
 }
